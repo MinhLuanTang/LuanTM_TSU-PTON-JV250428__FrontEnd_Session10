a42281097b782ae3ee4077b34ee3fe959c70e9de
MinhLuanTang <tmljp17101995@gmail.com>
2025-05-30 00:12:43 +0900

    nop bai 04

---
+++ ss10_bai04/bai04.js
@@ -0,0 +1,4 @@
+let FirstNumber = Number(prompt('Nhập giá trị số thứ nhất: '));
+let SecondNumber = Number(prompt('Nhập giá trị số thứ hai: '));
+let Total = FirstNumber + SecondNumber;
+alert('Tổng giá trị hai số là: ' + Total);
